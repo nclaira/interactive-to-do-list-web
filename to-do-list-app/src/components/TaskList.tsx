bcc37b9350ebd1a80d94ff93e456f03f0670ce73
nclaira <nclaire981@gmail.com>
2025-10-28 21:49:13 +0200

    create taskList.tsx in components

---
+++ to-do-list-app/src/components/TaskList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Droppable } from '@hello-pangea/dnd';
+import { Task } from '../types';
+import TaskItem from './TaskItem';
+
+interface TaskListProps {
+  tasks: Task[];
+  onToggleComplete: (id: string) => void;
+  onDelete: (id: string) => void;
+  onUpdateText: (id: string, newText: string) => void;
+}
+
+const TaskList: React.FC<TaskListProps> = ({
+  tasks,
+  onToggleComplete,
+  onDelete,
+  onUpdateText,
+}) => {
+  return (
+    <Droppable droppableId="tasks">
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          className="space-y-2"
+        >
+          {tasks.map((task, index) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              index={index}
+              onToggleComplete={onToggleComplete}
+              onDelete={onDelete}
+              onUpdateText={onUpdateText}
+            />
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
+
+export default TaskList;
